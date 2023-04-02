4b4a5b1513c81ac4ec44135c0f2e87d08fb07a95
Hoang Toan Nguyen <hoangtoannguyen@Toans-MacBook-Pro.local>
2023-04-02 17:04:16 +0700

    get listcate

--- src/main/java/vn/iotstar/controller/CategoryController.java
+++ src/main/java/vn/iotstar/controller/CategoryController.java
@@ -1,5 +1,24 @@
 package vn.iotstar.controller;
 
-public class CategoryController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.iotstar.entity.Category;
+import vn.iotstar.service.CategoryService;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+	
+	@Autowired
+	CategoryService cate;
+	
+	@GetMapping("list")
+	public List<Category> getCategories() {
+		return cate.findAll();
+	}
 }
