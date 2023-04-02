4d2550ff4d27c6b6468b4c125484c65bb48b4a69
Hoang Toan Nguyen <hoangtoannguyen@Toans-MacBook-Pro.local>
2023-04-02 18:32:13 +0700

    create api prodcut

--- src/main/java/vn/iotstar/controller/ProductController.java
+++ src/main/java/vn/iotstar/controller/ProductController.java
@@ -1,5 +1,25 @@
 package vn.iotstar.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.iotstar.entity.Product;
+import vn.iotstar.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+	
+	@Autowired
+	ProductService product;
+	
+	@GetMapping("list")
+	public List<Product> getProductAll() {
+		return product.findAll();
+	}
 
 }
