1d6f84982fb3608fee3b0bf080dcc08f4d676924
HNToan7202 <toannguyen7202@gmail.com>
2023-04-16 00:07:20 +0700

    create review

--- src/main/java/vn/iotstar/controller/ReviewController.java
+++ src/main/java/vn/iotstar/controller/ReviewController.java
@@ -1,5 +1,32 @@
 package vn.iotstar.controller;
 
-public class ReviewController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.iotstar.entity.Review;
+import vn.iotstar.service.ReviewService;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewController {
+	
+	@Autowired
+	ReviewService review;
+	
+	@GetMapping("list")
+	public List<Review> getReviewAll() {
+		return review.findAll();
+	}
+
+	@PostMapping("/add")
+	public Review addOrderItem(@RequestBody Review reiReview) {
+		return review.save(reiReview);
+	}
+	
 }
