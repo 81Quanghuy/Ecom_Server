33e81f98518fe77c1c05b46a74ab322800bf9d08
81QuangHuy <ngoquanghuy2210@gmail.com>
2023-04-16 23:35:35 +0700

    '16/4

--- src/main/java/vn/iotstar/controller/OrderItemController.java
+++ src/main/java/vn/iotstar/controller/OrderItemController.java
@@ -1,17 +1,21 @@
 package vn.iotstar.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.iotstar.entity.Order;
 import vn.iotstar.entity.OrderItem;
 import vn.iotstar.service.OrderItemService;
+import vn.iotstar.service.OrderService;
 
 @RestController
 @RequestMapping("/orderItem")
@@ -19,7 +23,9 @@ public class OrderItemController {
 	@Autowired
 	OrderItemService service;
 
-		
+	@Autowired
+	OrderService orderSerive;
+	
 	@PostMapping("/add")
 	public OrderItem addOrderItem(@RequestBody OrderItem orderItem) {
 		OrderItem entity = orderItem;
@@ -27,8 +33,21 @@ public class OrderItemController {
 		return service.save(orderItem);
 	}
 	@PostMapping("get")
-	public List<OrderItem> getAllList(@RequestBody Order order){
-		return service.findByOrder(order);
+	public List<OrderItem> getAllList(@RequestParam(name = "id") String id){
+		Optional<Order> entity = orderSerive.findById(id);
+		return service.findByOrder(entity.get());
 	}
 
+	@SuppressWarnings("null")
+	@PostMapping("getAll")
+	public List<OrderItem> getAll(@RequestBody List<Order> order){
+		List<OrderItem> list =  new ArrayList<OrderItem>();
+		for (Order entity : order) {
+			List<OrderItem> orderItem = service.findByOrder(entity);
+			for (OrderItem orderItem2 : orderItem) {
+				list.add(orderItem2);
+			}
+		}
+		return list;
+	}
 }
